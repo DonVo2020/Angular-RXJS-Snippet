a77ab450291c3e19a82d0069b519f16eff28e945
DonVoCache2020 <dunghuu_vo@hotmail.com>
2020-01-13 11:54:22 -0500

    Modified data.service.ts to handle forkJoin

--- data.service.ts
+++ data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { merge, Observable } from 'rxjs';
-import { delay } from 'rxjs/operators';
+import { merge, Observable, forkJoin } from 'rxjs';
+import { delay, map } from 'rxjs/operators';
 
 // constants that are used as pointers to some json-data
 const BOOKS: string = 'assets/data/books.json';
@@ -19,5 +19,17 @@ export class DataService {
     const authors = this.http.get(AUTHORS).pipe(delay(1000));
 
     return merge(books, authors);
+  }
+
+  getForkJoinData(): Observable<any> {
+    return forkJoin(
+      this.http.get(AUTHORS).pipe(delay(3000)),
+      this.http.get(BOOKS)
+    ).pipe(map((data: any[]) => {
+      let author: any = data[0][0]; // just get the first author from file. can do more with search/filter here.
+      let books: any[] = data[1]; // get all
+      author.books = books.filter(book => book.author === author.name);
+      return author;
+    }))
   }
 }
